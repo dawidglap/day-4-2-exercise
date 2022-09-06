823792755e1f7a7c320f3e25e9e35687abec6d3c
Dawid Glapiak <81537977+dawidglap@users.noreply.github.com>
2022-09-06 13:23:44 +0000

    Who pay the bill

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
-
 #Write your code below this line 👇
 random_int = random.randint(0, (len(names) - 1))
-print(f"{names[random_int]} is going to buy the meal today")
+print(f"{names[random_int]} is going to buy the meal today!")
+
