25cfde669e37a1ba5d1597ac3694fcfece46f222
Dawid Glapiak <81537977+dawidglap@users.noreply.github.com>
2022-09-06 13:22:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+# 🚨 Don't change the code below 👇
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+random_int = random.randint(0, (len(names) - 1))
+print(f"{names[random_int]} is going to buy the meal today")
